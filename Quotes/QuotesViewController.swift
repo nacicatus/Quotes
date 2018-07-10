79b58cb21c45cd464c2ab9c750b724b5fffa5994
Saurabh Sikka <saurabh.sikka@ericsson.com>
2018-07-10 12:29:25 +0200

    working software

--- Quotes/QuotesViewController.swift
+++ Quotes/QuotesViewController.swift
@@ -9,14 +9,25 @@
 import Cocoa
 
 class QuotesViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        currentQuoteIndex = 0
     }
     
     @IBOutlet weak var textLabel: NSTextField!
     
+    let quotes = Quote.all
+    
+    var currentQuoteIndex: Int = 0 {
+        didSet {
+            updateQuote()
+        }
+    }
+    
+    func updateQuote() {
+        textLabel.stringValue = String(describing: quotes[currentQuoteIndex])
+    }
     
     
     
@@ -37,14 +48,14 @@ extension QuotesViewController {
 // MARK: Actions
 extension QuotesViewController {
     @IBAction func previous(_ sender: NSButton) {
-        
+        currentQuoteIndex = (currentQuoteIndex - 1 + quotes.count) % quotes.count
     }
     
     @IBAction func next(_ sender: NSButton) {
-        
+        currentQuoteIndex = (currentQuoteIndex + 1) % quotes.count
     }
     
     @IBAction func quit(_ sender: NSButton) {
-    
+        NSApplication.shared().terminate(sender)
     }
 }
